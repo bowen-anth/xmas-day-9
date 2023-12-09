d29bbfb2fbcaa2ff1f0cb32e53e6e74aee3b32a0
bowen-anth <bowen.anth@gmail.com>
2023-12-09 15:54:24 -0700

    edited comments

--- index.js
+++ index.js
@@ -58,15 +58,5 @@ btnSend.addEventListener('click', handleSubmit);
 
     /** show dialog on load **/
     dialogModal.show()
-/**
- * 🎄 Challenge:
- * 1. When a user hits submit, dialogModal 
- *    should be hidden.
- * 2. Use the inputted text to generate an image 
- *    for the e-card using an AI model. 
- *    ⚠️ Make sure the image is square.
- * 3. Render the image to imageContainer.
- * 
- * 🎁 hint.md for help!
- **/   
+
 
